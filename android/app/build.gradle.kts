d4ea60b0cf937cc4f66b14ac97df25c6af6b5ab3
lolivier <louisolivier7602@gmail.com>
2025-05-23 12:08:19 +0200

    Adding sounds, the griedview and buttons to start a sound

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 android {
     namespace = "com.example.sound_board_italina_brain_rot"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
